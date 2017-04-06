c9affe0b0f5284c33aaeca07b5b33ba2b52d95c6
albahrani <albahrani@users.noreply.github.com>
2017-04-06 11:14:38 +0000

    Add license headers to java files.

--- src/main/java/com/github/albahrani/aquacontrol/server/rest/ServerLifecycleController.java
+++ src/main/java/com/github/albahrani/aquacontrol/server/rest/ServerLifecycleController.java
@@ -1,5 +1,5 @@
 /**
- * Copyright © 2015 albahrani (https://github.com/albahrani)
+ * Copyright © 2017 albahrani (https://github.com/albahrani)
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
