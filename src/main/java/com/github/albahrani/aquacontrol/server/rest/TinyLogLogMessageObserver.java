b92e1c1081f53804c7d2d8a0b21cd2061cc390bf
albahrani <github@a.no.spam.powerof4.de>
2017-05-01 20:37:20 +0200

    Improved TinyLogLogMessageObserver
    Avoid printing exception when RestExpress calls onException() after a
    onSuccess() for the same correlationId because the connection has been
    reset by peer.

--- src/main/java/com/github/albahrani/aquacontrol/server/rest/TinyLogLogMessageObserver.java
+++ src/main/java/com/github/albahrani/aquacontrol/server/rest/TinyLogLogMessageObserver.java
@@ -27,32 +27,42 @@ import org.restexpress.pipeline.MessageObserver;
 public class TinyLogLogMessageObserver extends MessageObserver {
 	private Map<String, Long> timers = new ConcurrentHashMap<>();
 
+	private Map<String, Boolean> successReceived = new ConcurrentHashMap<>();
+
 	@Override
 	protected void onReceived(Request request, Response response) {
 		timers.put(request.getCorrelationId(), System.currentTimeMillis());
-		Logger.info("{} {} {} received.", request.getCorrelationId(), request.getEffectiveHttpMethod(), request.getUrl());
+		successReceived.put(request.getCorrelationId(), false);
+		Logger.info("{} {} {} received.", request.getCorrelationId(), request.getEffectiveHttpMethod(),
+				request.getUrl());
 	}
 
 	@Override
 	protected void onException(Throwable exception, Request request, Response response) {
-		Logger.info("{} {} {} threw exception: {}", request.getCorrelationId(), request.getEffectiveHttpMethod(), request.getUrl(), exception.getMessage());
+		Boolean success = successReceived.get(request.getCorrelationId());
+		if (!Boolean.TRUE.equals(success)) {
+			Logger.info("{} {} {} threw exception: {}", request.getCorrelationId(), request.getEffectiveHttpMethod(),
+					request.getUrl(), exception.getMessage());
+		}
 	}
 
 	@Override
 	protected void onSuccess(Request request, Response response) {
-		Logger.info("{} {} {} was successfull.", request.getCorrelationId(), request.getEffectiveHttpMethod(), request.getUrl());
+		successReceived.put(request.getCorrelationId(), true);
+		Logger.info("{} {} {} was successfull.", request.getCorrelationId(), request.getEffectiveHttpMethod(),
+				request.getUrl());
 	}
 
 	@Override
 	protected void onComplete(Request request, Response response) {
+		successReceived.remove(request.getCorrelationId());
 		Long startTime = timers.remove(request.getCorrelationId());
 		Optional<String> duration = Optional.empty();
 		if (startTime != null) {
 			duration = Optional.of(String.valueOf(System.currentTimeMillis() - startTime) + "ms");
 		}
 
-		Logger.info("{} {} {} responded with {} in {}", request.getCorrelationId(), request.getEffectiveHttpMethod(), request.getUrl(),
-				response.getResponseStatus(),
-				duration.orElse("(no timer found)"));
+		Logger.info("{} {} {} responded with {} in {}", request.getCorrelationId(), request.getEffectiveHttpMethod(),
+				request.getUrl(), response.getResponseStatus(), duration.orElse("(no timer found)"));
 	}
 }
